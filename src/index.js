ecd82d4d8f536d534f7d2cbebc75bcc067b86a83
ipippuri <iiris.pippuri@gmail.com>
2018-01-27 21:47:36 +0200

    tehtavat 1.1 ja 1.2.

---
+++ src/index.js
@@ -0,0 +1,63 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => {
+  const kurssi = 'Half Stack -sovelluskehitys'
+  const osa1 = 'Reactin perusteet'
+  const tehtavia1 = 10
+  const osa2 = 'Tiedonvälitys propseilla'
+  const tehtavia2 = 7
+  const osa3 = 'Komponenttien tila'
+  const tehtavia3 = 14
+
+  return (
+    <div>
+      <Otsikko kurssi={kurssi} />
+      <Sisalto o1={osa1} t1={tehtavia1} o2={osa2} t2={tehtavia2} o3={osa3} t3={tehtavia3} />
+      <Yhteensa t1={tehtavia1}  t2={tehtavia2}  t3={tehtavia3} />
+    </div>
+  )
+}
+
+
+const Otsikko = (props) => {
+    return (
+      <div>
+        <h1>{props.kurssi}</h1>
+      </div>
+    )
+  }
+
+  const Sisalto = (props) => {
+    return (
+      <div>
+      <Osa osa={props.o1} tehtavia={props.t1} />
+      <Osa osa={props.o2} tehtavia={props.t2} />
+      <Osa osa={props.o3} tehtavia={props.t3} />
+      </div>
+    )
+  }
+
+  const Osa = (props) => {
+    return (
+      <div>
+      <p>{props.osa} {props.tehtavia}</p>
+      </div>
+    )
+  }
+
+
+
+  const Yhteensa = (props) => {
+    return (
+      <div>
+        <p>yhteensä {props.t1 + props.t2 + props.t3} tehtävää</p>
+      </div>
+    )
+  }
+
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
